8a8ff9a70197840fcce92b58cd66fa92ba7772b0
sensibiloper <sensibiloper@gmail.com>
2017-02-23 19:39:37 +0900

    cafe 24 upload

---
+++ credentials.js
@@ -0,0 +1,12 @@
+module.exports = {
+  cookie : {secret : 'sensibiloperSblog'},
+  session : {
+              secret : 'sensibiloperSblog',
+              resave : false,
+              saveUninitialized : true
+            },
+  admin : {
+              id : 'sensibiloper',
+              password : '1234'
+            }
+};
--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 var work = require('./api/work');
+var about = require('./api/about');
+var study = require('./api/study');
 var blog = require('./api/blog');
 var contact = require('./api/contact');
 
 router.use('/work', work);
+router.use('/about', about);
+router.use('/study', study);
 router.use('/blog', blog);
 router.use('/contact', contact);
 
